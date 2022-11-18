ae463741ee517a8249cfa7ecb079a5d3583e5278
Muhammad Hanif Kusuma <hanif.kusuma@shopee.com>
2022-11-18 17:35:43 +0700

    fix: fixed user model misstype

--- final-project-backend/model/user.go
+++ final-project-backend/model/user.go
@@ -8,12 +8,12 @@ import (
 
 type User struct {
 	Id             int            `gorm:"primaryKey; column:id"`
-	Fullname       string         `gorm:"column:fullname"`
+	FullName       string         `gorm:"column:full_name"`
 	Phone          string         `gorm:"column:phone"`
 	Email          string         `gorm:"column:email"`
 	Username       string         `gorm:"column:username"`
 	Password       string         `gorm:"column:password"`
-	Role           string         `gorm:"column:role"`
+	Role           int            `gorm:"column:role"`
 	ProfilePicture string         `gorm:"column:profile_picture"`
 	CreatedAt      time.Time      `gorm:"column:created_at"`
 	UpdatedAt      time.Time      `gorm:"column:updated_at"`
