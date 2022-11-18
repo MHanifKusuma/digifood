98e4ad7fd98f46b91181d202c3239625b7ab4e7c
Muhammad Hanif Kusuma <hanif.kusuma@shopee.com>
2022-11-18 17:44:26 +0700

    test: added authentication service unit tests [40.9%]

---
+++ final-project-backend/service/auth_test.go
@@ -0,0 +1,98 @@
+package service
+
+import (
+	"final-project-backend/mocks"
+	"final-project-backend/model"
+	"final-project-backend/pkg/utils"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestUserLogin(t *testing.T) {
+	t.Run("success", func(t *testing.T) {
+		loginUser := model.UserLogin{
+			Login:    "user.one@email.com",
+			Password: "1234",
+		}
+
+		authRepo := mocks.NewAuthRepository(t)
+		authRepo.On("FindUserByLoginInfo", loginUser).Return(&model.User{
+			Email:    "user.one@email.com",
+			Password: `$2a$04$LoG0NFPuaobuUM0X9j4PGOU8PNxlcdaO7CHckjOcNPPotR6EKNWQq`,
+		}, nil)
+
+		authService := NewAuthService(authRepo)
+		token, status, _ := authService.Login(&loginUser)
+
+		assert.Equal(t, 200, status)
+		assert.NotNil(t, token)
+
+	})
+
+	t.Run("user-not-found", func(t *testing.T) {
+		loginUser := model.UserLogin{
+			Login:    "user.two@email.com",
+			Password: "1234",
+		}
+
+		authRepo := mocks.NewAuthRepository(t)
+		authRepo.On("FindUserByLoginInfo", loginUser).Return(nil, utils.ErrUserNotFound)
+
+		authService := NewAuthService(authRepo)
+		token, status, _ := authService.Login(&loginUser)
+
+		assert.Equal(t, 404, status)
+		assert.Equal(t, "", token)
+
+	})
+
+	t.Run("wrong-credentials", func(t *testing.T) {
+		loginUser := model.UserLogin{
+			Login:    "user.one@email.com",
+			Password: "0987",
+		}
+
+		authRepo := mocks.NewAuthRepository(t)
+		authRepo.On("FindUserByLoginInfo", loginUser).Return(&model.User{
+			Email:    "user.one@email.com",
+			Password: `$2a$04$LoG0NFPuaobuUM0X9j4PGOU8PNxlcdaO7CHckjOcNPPotR6EKNWQq`,
+		}, nil)
+
+		authService := NewAuthService(authRepo)
+		token, status, _ := authService.Login(&loginUser)
+
+		assert.Equal(t, 403, status)
+		assert.Equal(t, "", token)
+
+	})
+}
+
+// func TestUserRegister(t *testing.T) {
+// 	t.Run("success", func(t *testing.T) {
+// 		registerUser := model.User{
+// 			FullName:  "usernew",
+// 			Email:     "user.new@email.com",
+// 			Phone:     "098765654321",
+// 			Username:  "usernew",
+// 			Password:  "usernew",
+// 			Role:      1,
+// 			CreatedAt: time.Now(),
+// 			UpdatedAt: time.Now(),
+// 		}
+
+// 		authRepo := mocks.NewAuthRepository(t)
+
+// 		checkUser := model.UserLogin{
+// 			Login:    registerUser.Email,
+// 			Password: registerUser.Password,
+// 		}
+// 		authRepo.On("FindUserByLoginInfo", checkUser).Return(nil, utils.ErrUserNotFound)
+// 		authRepo.On("RegisterUser", registerUser).Return("register success", nil)
+
+// 		authService := NewAuthService(authRepo)
+// 		_, status, _ := authService.RegisterUser(&registerUser)
+
+// 		assert.Equal(t, 201, status)
+// 	})
+// }
