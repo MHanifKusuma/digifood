74112c059df6ea9b90b51bca81be49d7a73e88f3
Muhammad Hanif Kusuma <hanif.kusuma@shopee.com>
2022-11-18 17:42:43 +0700

    feat: added authentication utilities

---
+++ final-project-backend/pkg/utils/jwt/jwt.go
@@ -0,0 +1,58 @@
+package jwt
+
+import (
+	"time"
+
+	"github.com/golang-jwt/jwt/v4"
+)
+
+var (
+	secret      = "muhammadhanifkusumafinalprojectsecretkey"
+	jwtDuration = 1 * time.Hour
+)
+
+type CustomClaims struct {
+	UserId string `json:"user_id"`
+	jwt.RegisteredClaims
+}
+
+func GenerateToken(userId string) (string, error) {
+	now := time.Now()
+
+	claims := CustomClaims{
+		userId,
+		jwt.RegisteredClaims{
+			IssuedAt:  jwt.NewNumericDate(now),
+			ExpiresAt: jwt.NewNumericDate(now.Add(jwtDuration)),
+			Issuer:    "issuerforjwt",
+		},
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	ss, err := token.SignedString([]byte(secret))
+	if err != nil {
+		return "", nil
+	}
+
+	return ss, nil
+}
+
+func ValidateToken(input string) (string, error) {
+	token, err := jwt.ParseWithClaims(
+		input,
+		&CustomClaims{},
+		func(t *jwt.Token) (interface{}, error) {
+			return []byte(secret), nil
+		},
+	)
+
+	if err != nil {
+		return "", nil
+	}
+
+	if claims, ok := token.Claims.(*CustomClaims); ok && token.Valid {
+		return claims.UserId, nil
+	} else {
+		return "", err
+	}
+}
