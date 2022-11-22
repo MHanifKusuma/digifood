3209c634749b7b7d8264d5d7894362cf1dda75a2
Muhammad Hanif Kusuma <hanif.kusuma@shopee.com>
2022-11-22 17:14:10 +0700

    feat: added menu list on category model

--- final-project-backend/model/category.go
+++ final-project-backend/model/category.go
@@ -7,8 +7,9 @@ import (
 )
 
 type Category struct {
-	Id        int            `gorm:"column:id"`
+	Id        int            `gorm:"primaryKey;column:id"`
 	Name      string         `gorm:"column:name"`
+	Menu      []Menu         `gorm:"foreignKey:CategoryId;references:Id"`
 	CreatedAt time.Time      `gorm:"column:created_at"`
 	UpdatedAt time.Time      `gorm:"column:updated_at"`
 	DeletedAt gorm.DeletedAt `gorm:"column:deleted_at"`
