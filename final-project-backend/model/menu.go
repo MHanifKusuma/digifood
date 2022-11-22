d246895148bd66346e6a6353f9b9ad4b0f2b188a
Muhammad Hanif Kusuma <hanif.kusuma@shopee.com>
2022-11-22 17:13:50 +0700

    feat: added menu model

---
+++ final-project-backend/model/menu.go
@@ -0,0 +1,23 @@
+package model
+
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
+
+type Menu struct {
+	Id             int            `gorm:"primarykey;column:id"`
+	CategoryId     int            `gorm:"column:category_id"`
+	Name           string         `gorm:"column:name"`
+	Description    string         `gorm:"column:description"`
+	AverageRating  float64        `gorm:"column:avg_rating"`
+	TotalFavorites int            `gorm:"total_favorites"`
+	Price          int            `gorm:"column:price"`
+	MenuPhoto      string         `gorm:"column:menu_photo"`
+	MenuOptions    []MenuOption   `gorm:"foreignKey:MenuId;references:Id"`
+	Promotion      Promotion      `gorm:"foreignKey:MenuId;references:Id"`
+	CreatedAt      time.Time      `gorm:"column:created_at"`
+	UpdatedAt      time.Time      `gorm:"column:updated_at"`
+	DeletedAt      gorm.DeletedAt `gorm:"column:deleted_at"`
+}
