8254067ee887c0b2500daf3ff782384f40939881
Muhammad Hanif Kusuma <hanif.kusuma@shopee.com>
2022-11-22 17:14:24 +0700

    feat: added menu options model

---
+++ final-project-backend/model/menu_options.go
@@ -0,0 +1,17 @@
+package model
+
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
+
+type MenuOption struct {
+	Id        int            `gorm:"primaryKey; column:id"`
+	Name      string         `gorm:"column:name"`
+	MenuId    int            `gorm:"column:menu_id"`
+	Price     int            `gorm:"column:price"`
+	CreatedAt time.Time      `gorm:"column:created_at"`
+	UpdatedAt time.Time      `gorm:"column:updated_at"`
+	DeletedAt gorm.DeletedAt `gorm:"column:deleted_at"`
+}
