ebbf32a11aa664333edbd739e22dcda92e7ffb8f
Muhammad Hanif Kusuma <hanif.kusuma@shopee.com>
2022-11-23 17:36:57 +0700

    refactor: moved page components into seperated folder

--- final-project-frontend/src/pages/public/Home/index.tsx
+++ final-project-frontend/src/pages/public/Home/index.tsx
@@ -1,8 +1,8 @@
 import Navbar from "components/shared-components/Navbar";
 import React, { useEffect, useState } from "react";
 import HomeWrapper from "./style";
-import Hero from "components/HomeComponent/Hero";
-import OurMenu from "components/HomeComponent/OurMenu";
+import Hero from "components/page-components/HomeComponent/Hero";
+import OurMenu from "components/page-components/HomeComponent/OurMenu";
 import { IMenuByCategory } from "interfaces/Menu";
 import axios, { AxiosResponse } from "axios";
 
