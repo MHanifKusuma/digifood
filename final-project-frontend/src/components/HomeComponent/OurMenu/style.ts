f082739c19786b45c82087ee54632aa23e31ae2c
Muhammad Hanif Kusuma <hanif.kusuma@shopee.com>
2022-11-23 16:06:54 +0700

    feat: added our menu section in home page

---
+++ final-project-frontend/src/components/HomeComponent/OurMenu/style.ts
@@ -0,0 +1,63 @@
+import styled from "styled-components";
+import { lighterColor } from "utils/index";
+
+const OurMenuWrapper = styled.section`
+  min-height: 100vh;
+  background-color: #f8f8f8;
+`;
+
+export const CategoryWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-content: start;
+  flex-wrap: wrap;
+  overflow-x: scroll;
+  overflow-y: hidden;
+  height: 11rem;
+
+  img {
+    width: 80%;
+    height: 80%;
+  }
+
+  ::-webkit-scrollbar {
+    height: 10px;
+  }
+
+  ::-webkit-scrollbar-track {
+    box-shadow: inset 0 0 2px grey;
+    border-radius: 10px;
+  }
+
+  ::-webkit-scrollbar-thumb {
+    background: #d9d9d9;
+    border-radius: 10px;
+  }
+
+  ::-webkit-scrollbar-thumb:hover {
+    background: #d3d3d3;
+  }
+
+  .cat-image {
+    border-radius: 50%;
+    width: 150px;
+    height: 150px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    transition: all 0.2s ease-in-out;
+  }
+
+  .active-cat {
+    background-color: #579eff;
+  }
+`;
+
+export const MenuWrapper = styled.div`
+  margin-top: 2rem;
+  padding: 5rem 2rem;
+  background-color: #ffe09c;
+  border-radius: 50px;
+`;
+
+export default OurMenuWrapper;
--- final-project-frontend/src/pages/public/Home/index.tsx
+++ final-project-frontend/src/pages/public/Home/index.tsx
@@ -1,15 +1,29 @@
 import Navbar from "components/Navbar";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import HomeWrapper from "./style";
-import Logo from "assets/logo.webp";
 import Hero from "components/HomeComponent/Hero";
+import OurMenu from "components/HomeComponent/OurMenu";
+import { IMenuByCategory } from "interfaces/Menu";
+import axios, { AxiosResponse } from "axios";
 
 const Home = () => {
+  const [categories, setCategories] = useState<IMenuByCategory[]>([]);
+
+  const FetchMenuByCategory = () => {
+    axios
+      .get("http://localhost:8080/menus/categories")
+      .then((data) => setCategories(data.data.data));
+  };
+
+  useEffect(() => {
+    FetchMenuByCategory();
+  }, []);
   return (
     <>
       <Navbar />
       <HomeWrapper>
         <Hero />
+        <OurMenu categories={categories} />
       </HomeWrapper>
     </>
   );
