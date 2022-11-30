76b01f3836e82c4010c304aea2e301c458a6a830
Muhammad Hanif Kusuma <hanif.kusuma@shopee.com>
2022-11-30 09:17:39 +0700

    feat: added user favorites model

--- final-project-backend/model/user.go
+++ final-project-backend/model/user.go
@@ -7,15 +7,28 @@ import (
 )
 
 type User struct {
-	Id             int            `gorm:"primaryKey; column:id"`
-	FullName       string         `gorm:"column:full_name"`
-	Phone          string         `gorm:"column:phone"`
-	Email          string         `gorm:"column:email"`
-	Username       string         `gorm:"column:username"`
-	Password       string         `gorm:"column:password"`
-	Role           int            `gorm:"column:role"`
-	ProfilePicture string         `gorm:"column:profile_picture"`
-	CreatedAt      time.Time      `gorm:"column:created_at"`
-	UpdatedAt      time.Time      `gorm:"column:updated_at"`
-	DeletedAt      gorm.DeletedAt `gorm:"column:deleted_at"`
+	Id             int             `gorm:"primaryKey; column:id"`
+	FullName       string          `gorm:"column:full_name"`
+	Phone          string          `gorm:"column:phone"`
+	Email          string          `gorm:"column:email"`
+	Username       string          `gorm:"column:username"`
+	Password       string          `gorm:"column:password"`
+	Role           int             `gorm:"column:role"`
+	ProfilePicture string          `gorm:"column:profile_picture"`
+	UserFavorite   []UserFavorites `gorm:"foreignKey:UserId;references:Id"`
+	CreatedAt      time.Time       `gorm:"column:created_at"`
+	UpdatedAt      time.Time       `gorm:"column:updated_at"`
+	DeletedAt      gorm.DeletedAt  `gorm:"column:deleted_at"`
+}
+
+type UserFavorites struct {
+	Id     int  `gorm:"primaryKey;column:id"`
+	UserId int  `gorm:"column:user_id"`
+	MenuId int  `gorm:"column:menu_id"`
+	Menu   Menu `gorm:"foreignKey:Id;references:MenuId"`
+}
+
+type NewUserFavorite struct {
+	UserId int
+	MenuId int `json:"menuId"`
 }
